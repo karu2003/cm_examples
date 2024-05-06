160f1a79b2a289df8b50daa6f25af7db5c1a1872
karu2003 <ka_ru2003@msn.com>
2024-05-06 12:32:25 +0000

    add params subclass

--- generator/gen_ui.py
+++ generator/gen_ui.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtCore import Qt
 
 import matplotlib.pyplot as plt
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui
 from PyQt5.QtWidgets import QSizePolicy
-import pyqtgraph.parametertree.parameterTypes as pTypes
-from pyqtgraph.parametertree import Parameter, ParameterTree
-import pyqtgraph.parametertree as ptree
+
 import sys
 import time
 import os
@@ -20,94 +17,12 @@ import socket
 import struct
 from enum import Enum
 
-try:
-    import pyaudio  # sudo apt-get install python3-pyaudio
-except ImportError:
-    pyaudio = None
 
-from params import params
+from params import GenParameterTree, FreqType, SAMPLE_FORMATS, PLAYERS, FORMATS, pyaudio
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 uiclass, baseclass = pg.Qt.loadUiType(__location__ + "/ui_gen.ui")
-
-TreeStyle = """
-            QTreeView {
-                background-color: rgb(46, 52, 54);
-                alternate-background-color: rgb(39, 44, 45);
-                color: rgb(238, 238, 238);
-            }
-            QLabel {
-                color: rgb(238, 238, 238);
-            }
-            QTreeView::item:has-children {
-                background-color: '#212627';
-                color: rgb(233, 185, 110);
-            }
-            QTreeView::item:selected {
-                background-color: rgb(92, 53, 102);
-            }
-            """
-
-
-@contextlib.contextmanager
-def WavFileWriter(filename, sample_format, sample_rate_hz):
-    stdout = filename == "-"
-    with wave.open(sys.stdout.buffer if stdout else filename, "wb") as f:
-        f.setnchannels(1)
-        f.setsampwidth(sample_format.bytes)
-        f.setframerate(sample_rate_hz)
-        try:
-            yield f.writeframes
-        finally:
-            if stdout:
-                return
-            print("File [WAV]:", filename)
-            print("Sample rate (Hz):", f.getframerate())
-            print("Sample size (bytes):", f.getsampwidth())
-            print("Sample count:", f.getnframes())
-            print("Duration (ms):", 1000.0 * f.getnframes() / f.getframerate())
-
-
-@contextlib.contextmanager
-def RawFileWriter(filename, sample_format, sample_rate_hz):
-    if filename == "-":
-        yield sys.stdout.buffer.write
-    else:
-        with open(filename, "wb") as f:
-            try:
-                yield f.write
-            finally:
-                num_samples = f.tell() / sample_format.bytes
-                print("File [RAW]:", filename)
-                print("Sample rate (Hz):", sample_rate_hz)
-                print("Sample size (bytes):", sample_format.bytes)
-                print("Sample count:", num_samples)
-                print("Duration (ms):", 1000.0 * num_samples / sample_rate_hz)
-
-
-@contextlib.contextmanager
-def Null(*args, **kwargs):
-    yield lambda *largs: None
-
-
-SampleFormat = collections.namedtuple("SampleFormat", ["name", "id", "bytes", "ffplay"])
-SAMPLE_FORMATS = {
-    f.name: f
-    for f in [
-        SampleFormat(name="S16_LE", id=0, bytes=2, ffplay="s16le"),
-        SampleFormat(name="S32_LE", id=1, bytes=4, ffplay="s32le"),
-    ]
-}
-PLAYERS = (
-    {"blocking": BlockingMonoPlayer, "callback": CallbackMonoPlayer, "none": Null}
-    if pyaudio
-    else {"none": Null}
-)
-FORMATS = {"raw": RawFileWriter, "wav": WavFileWriter}
-
-HIDE = ["host","port","output", "ffplay", "player","format"]
-HZ_SUFFIX = ["sample_rate_hz", "dac_sample_rate_hz", "fstart", "fstop"]
 
 
 def str2bool(v):
@@ -121,55 +36,13 @@ def str2bool(v):
         raise argparse.ArgumentTypeError("Boolean value expected.")
 
 
-class FreqType(Enum):
-    linear = 0
-    quad = 1
-    log = 2
-    sin = 3
-    triangle = 4
-
-    def __str__(self):
-        return self.name
-
-    @staticmethod
-    def from_string(s):
-        try:
-            return FreqType[s]
-        except KeyError:
-            raise ValueError()
-
-
-
 class MainWindow(uiclass, baseclass, object):
-    def __init__(self,):
+    def __init__(self, args):
         self.app = QtWidgets.QApplication(sys.argv)
         super().__init__()
         self.setupUi(self)
-        self.params = params
 
-        # self.p = ptree.Parameter.create(name="Parameters", type="group")
-        # self.t = ptree.ParameterTree(showHeader=False)
-        # self.t.setParameters(self.p, showTop=False)        
-        # self.p.child("args").setOpts(title="Genarator Options")
-
-        # self.p = Parameter.create(name="params", type="group", children=self.params)
-        # self.p = self.init_params_from_args(args, params=self.params)
-        self.p = self.create_params_from_args(args)
-        self.hide_params(self.p)
-        self.add_suxffix(self.p, suffix_list=HZ_SUFFIX, suffix="Hz")
-        self.add_sisuffix(self.p, si_list=HZ_SUFFIX)
-        self.t = ParameterTree(showHeader=False)
-        self.t.setParameters(self.p, showTop=False)
-        
-        self.p.sigTreeStateChanged.connect(self.change)
-        self.t.setStyleSheet(TreeStyle)
-
-        # pl = self.t.palette()
-        # pl.setColor(self.t.backgroundRole(), Qt.red)
-        # self.t.setPalette(pl)
-
-        # self.t.addParameters(self.p)
-        # self.t.setWindowTitle("Generator Parameter")
+        self.tree = GenParameterTree(args)
 
         # self.showMaximized()
         # self.showFullScreen()
@@ -185,113 +58,14 @@ class MainWindow(uiclass, baseclass, object):
         self.measurements.hideAxis("bottom")
 
         self.vb_w = self.measurements.getViewBox()
-        self.t.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.vlay_tree.addWidget(self.t, stretch=7)
+        self.tree.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.vlay_tree.addWidget(self.tree, stretch=7)
         # self.vlay_tree.addStretch()
         self.button = QtWidgets.QPushButton("Start")
         self.button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.button.clicked.connect(self.ButtonStart)
         self.vlay_tree.addWidget(self.button, stretch=1)
         # self.vlay_tree.addStretch()
-    
-    def init_params_from_args(self, args, params):
-        """Initialize parameters from command line arguments."""
-        p = Parameter.create(name="params", type="group", children=params)
-        args = vars(args)
-        for k, v in args.items():
-            if k in p.names:
-                p[k] = v
-        return p
-    
-    def create_params_from_args(self, args):
-        """Create parameters from command line arguments use .setOpts."""
-        p = Parameter.create(name="params", type="group")
-        args = vars(args)
-        for k, v in args.items():
-            if type(v) == bool:
-                p.addChild({"name": k, "type": "bool", "value": v})
-            elif type(v) == int:
-                p.addChild({"name": k, "type": "int", "value": v})
-            elif type(v) == float:
-                p.addChild({"name": k, "type": "float", "value": v})
-            elif type(v) == str:
-                if k == "format":
-                    p.addChild(
-                        {
-                            "name": k,
-                            "type": "list",
-                            "limits": list(FORMATS.keys()),
-                            "value": v,
-                        }
-                    )
-                else:
-                    p.addChild({"name": k, "type": "str", "value": v})
-                # p.addChild({"name": k, "type": "str", "value": v})
-            elif type(v) == FreqType:
-                p.addChild(
-                    {
-                        "name": k,
-                        "type": "list",
-                        "limits": ["linear", "quad", "log", "sin", "triangle"],
-                        "value": str(v),
-                    }
-                )
-            elif type(v) == SampleFormat:
-                p.addChild(
-                    {
-                        "name": k,
-                        "type": "list",
-                        "limits": list(SAMPLE_FORMATS.keys()),
-                        "value": v.name,
-                    }
-                )
-        return p
-    
-    def hide_params(self, p, hide=HIDE):
-        """Hide some parameters."""
-        for k in p.names:
-            if k in hide:
-                p.param(k).setOpts(visible=False)
-                
-    def add_suxffix(self, p, suffix_list = HZ_SUFFIX, suffix="Hz"):
-        """Add suffix to list parameters."""
-        for k in p.names:
-            if k in suffix_list:
-                k = p.param(k).setOpts(suffix=suffix)
-    
-    def add_sisuffix(self, p, si_list = HZ_SUFFIX):
-        """Add SI suffix to list parameters."""
-        for k in p.names:
-            if k in si_list:
-                k = p.param(k).setOpts(siPrefix=True)
-
-    def change(self, param, changes):
-        print("tree changes:")
-        for param, change, data in changes:
-            path = self.p.childPath(param)
-            if path is not None:
-                childName = ".".join(path)
-            else:
-                childName = param.name()
-            print("  parameter: %s" % childName)
-            print("  change:    %s" % change)
-            print("  data:      %s" % str(data))
-            print("  ----------")
-
-    def valueChanging(self, param, value):
-        print("Value changing (not finalized): %s %s" % (param, value))
-
-    def save(self):
-        global state
-        state = self.p.saveState()
-
-    def restore(self):
-        global state
-        add = self.p["Save/Restore functionality", "Restore State", "Add missing items"]
-        rem = self.p[
-            "Save/Restore functionality", "Restore State", "Remove extra items"
-        ]
-        self.p.restoreState(state, addChildren=add, removeChildren=rem)
 
     def ButtonStart(self):
         print("Start")
@@ -469,7 +243,7 @@ if __name__ == "__main__":
 
     g_start = 1
 
-    main = MainWindow()
+    main = MainWindow(args)
     main.show()
     main.animation()
     print("Exiting...")
--- generator/params.py
+++ generator/params.py
@@ -1,3 +1,11 @@
+import collections
+import contextlib
+
+try:
+    import pyaudio  # sudo apt-get install python3-pyaudio
+except ImportError:
+    pyaudio = None
+
 params = [
     {
         "name": "Sample Rate",
@@ -88,5 +96,240 @@ params = [
     },
     {"name": "Auto restart", "type": "bool", "value": True},
     {"name": "Run back", "type": "bool", "value": False},
-    {"name": "Start", "type": "bool", "value": False}#, "visible":False},
+    {"name": "Start", "type": "bool", "value": False},  # , "visible":False},
 ]
+
+
+HIDE = ["host", "port", "output", "ffplay", "player", "format"]
+HZ_SUFFIX = ["sample_rate_hz", "dac_sample_rate_hz", "fstart", "fstop"]
+
+from enum import Enum
+
+
+class FreqType(Enum):
+    linear = 0
+    quad = 1
+    log = 2
+    sin = 3
+    triangle = 4
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def from_string(s):
+        try:
+            return FreqType[s]
+        except KeyError:
+            raise ValueError()
+
+
+@contextlib.contextmanager
+def Null(*args, **kwargs):
+    yield lambda *largs: None
+
+
+@contextlib.contextmanager
+def WavFileWriter(filename, sample_format, sample_rate_hz):
+    stdout = filename == "-"
+    with wave.open(sys.stdout.buffer if stdout else filename, "wb") as f:
+        f.setnchannels(1)
+        f.setsampwidth(sample_format.bytes)
+        f.setframerate(sample_rate_hz)
+        try:
+            yield f.writeframes
+        finally:
+            if stdout:
+                return
+            print("File [WAV]:", filename)
+            print("Sample rate (Hz):", f.getframerate())
+            print("Sample size (bytes):", f.getsampwidth())
+            print("Sample count:", f.getnframes())
+            print("Duration (ms):", 1000.0 * f.getnframes() / f.getframerate())
+
+
+@contextlib.contextmanager
+def RawFileWriter(filename, sample_format, sample_rate_hz):
+    if filename == "-":
+        yield sys.stdout.buffer.write
+    else:
+        with open(filename, "wb") as f:
+            try:
+                yield f.write
+            finally:
+                num_samples = f.tell() / sample_format.bytes
+                print("File [RAW]:", filename)
+                print("Sample rate (Hz):", sample_rate_hz)
+                print("Sample size (bytes):", sample_format.bytes)
+                print("Sample count:", num_samples)
+                print("Duration (ms):", 1000.0 * num_samples / sample_rate_hz)
+
+
+SampleFormat = collections.namedtuple("SampleFormat", ["name", "id", "bytes", "ffplay"])
+SAMPLE_FORMATS = {
+    f.name: f
+    for f in [
+        SampleFormat(name="S16_LE", id=0, bytes=2, ffplay="s16le"),
+        SampleFormat(name="S32_LE", id=1, bytes=4, ffplay="s32le"),
+    ]
+}
+
+
+PLAYERS = (
+    {"blocking": BlockingMonoPlayer, "callback": CallbackMonoPlayer, "none": Null}
+    if pyaudio
+    else {"none": Null}
+)
+FORMATS = {"raw": RawFileWriter, "wav": WavFileWriter}
+
+TreeStyle = """
+            QTreeView {
+                background-color: rgb(46, 52, 54);
+                alternate-background-color: rgb(39, 44, 45);
+                color: rgb(238, 238, 238);
+            }
+            QLabel {
+                color: rgb(238, 238, 238);
+            }
+            QTreeView::item:has-children {
+                background-color: '#212627';
+                color: rgb(233, 185, 110);
+            }
+            QTreeView::item:selected {
+                background-color: rgb(92, 53, 102);
+            }
+            """
+
+
+from pyqtgraph.parametertree import Parameter, ParameterTree
+from PyQt5.QtCore import Qt
+
+
+class GenParameterTree(ParameterTree):
+    def __init__(self, args):
+        super().__init__()
+        # self.params = params
+        self.p = self.init_params_from_args(args, params)
+        self.p = self.create_params_from_args(args)
+
+        self.hide_params(self.p)
+        self.add_suxffix(self.p, suffix_list=HZ_SUFFIX, suffix="Hz")
+        self.add_sisuffix(self.p, si_list=HZ_SUFFIX)
+
+        self.p.sigTreeStateChanged.connect(self.change)
+        self.setParameters(self.p, showTop=False)
+        # self.setStyleSheet(TreeStyle)
+
+        pl = self.palette()
+        pl.setColor(self.backgroundRole(), Qt.red)
+        self.setPalette(pl)
+
+    def init_params_from_args(self, args, params):
+        """Initialize parameters from command line arguments."""
+        p = Parameter.create(name="params", type="group", children=params)
+        args = vars(args)
+        for k, v in args.items():
+            if k in p.names:
+                p[k] = v
+        return p
+
+    def create_params_from_args(self, args):
+        """Create parameters from command line arguments use .setOpts."""
+        p = Parameter.create(name="params", type="group")
+        args = vars(args)
+        for k, v in args.items():
+            if type(v) == bool:
+                p.addChild({"name": k, "type": "bool", "value": v})
+            elif type(v) == int:
+                p.addChild({"name": k, "type": "int", "value": v})
+            elif type(v) == float:
+                p.addChild({"name": k, "type": "float", "value": v})
+            elif type(v) == str:
+                if k == "format":
+                    p.addChild(
+                        {
+                            "name": k,
+                            "type": "list",
+                            "limits": list(FORMATS.keys()),
+                            "value": v,
+                        }
+                    )
+                else:
+                    p.addChild({"name": k, "type": "str", "value": v})
+                # p.addChild({"name": k, "type": "str", "value": v})
+            elif type(v) == FreqType:
+                p.addChild(
+                    {
+                        "name": k,
+                        "type": "list",
+                        "limits": ["linear", "quad", "log", "sin", "triangle"],
+                        "value": str(v),
+                    }
+                )
+            elif type(v) == SampleFormat:
+                p.addChild(
+                    {
+                        "name": k,
+                        "type": "list",
+                        "limits": list(SAMPLE_FORMATS.keys()),
+                        "value": v.name,
+                    }
+                )
+        return p
+
+    def hide_params(self, p, hide=HIDE):
+        """Hide some parameters."""
+        for k in p.names:
+            if k in hide:
+                p.param(k).setOpts(visible=False)
+
+    def add_suxffix(self, p, suffix_list=HZ_SUFFIX, suffix="Hz"):
+        """Add suffix to list parameters."""
+        for k in p.names:
+            if k in suffix_list:
+                k = p.param(k).setOpts(suffix=suffix)
+
+    def add_sisuffix(self, p, si_list=HZ_SUFFIX):
+        """Add SI suffix to list parameters."""
+        for k in p.names:
+            if k in si_list:
+                k = p.param(k).setOpts(siPrefix=True)
+
+    def change(self, param, changes):
+        print("tree changes:")
+        for param, change, data in changes:
+            path = self.p.childPath(param)
+            if path is not None:
+                childName = ".".join(path)
+            else:
+                childName = param.name()
+            print("  parameter: %s" % childName)
+            print("  change:    %s" % change)
+            print("  data:      %s" % str(data))
+            print("  ----------")
+
+    def valueChanging(self, param, value):
+        print("Value changing (not finalized): %s %s" % (param, value))
+
+    def save(self):
+        global state
+        state = self.p.saveState()
+
+    def restore(self):
+        global state
+        add = self.p["Save/Restore functionality", "Restore State", "Add missing items"]
+        rem = self.p[
+            "Save/Restore functionality", "Restore State", "Remove extra items"
+        ]
+        self.p.restoreState(state, addChildren=add, removeChildren=rem)
+
+    def print_all_set(self, p):
+        """print all settings of paramert"""
+        state = p.saveState()
+        for param in p:
+            # print(param.opts)
+            for i in param.opts:
+                print(i)
+        # print(state)
+        # for i in p.names:
+        #     print(type(p.param(i)).opts)
